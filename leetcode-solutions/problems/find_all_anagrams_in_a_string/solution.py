1276977832c598d64cec6e2696f636b0e5ab0d06
Haikux <harishrsk11@gmail.com>
2023-07-11 08:24:14 +0000

    Sync LeetCode submission - Find All Anagrams in a String (python3)

--- leetcode-solutions/problems/find_all_anagrams_in_a_string/solution.py
+++ leetcode-solutions/problems/find_all_anagrams_in_a_string/solution.py
@@ -1,46 +1,42 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        freq_table = {}
+        freq_map = {}
         res = []
+        counter = 0
 
-        if (len(s) < len(p) or len(s) == 0):
-            return res
-        #Build frequency table for p
         for i in p:
-            if i not in freq_table:
-                freq_table[i] = 1
+            if i in freq_map:
+                freq_map[i] += 1
             else:
-                freq_table[i] += 1
+                freq_map[i] = 1
+                counter += 1
 
-        counter = len(freq_table)
-        #Use start and end pointers
+        if len(s) < len(p) or len(p) == 0:
+            return res
+        
         start, end = 0, 0
-        l = len(p)
-        #First move the end pointer to right, until the counter is 0
-        while(end<len(s)):
+        
+        while(end < len(s)):
             e = s[end]
 
-            if e in freq_table:
-                freq_table[e] -= 1
-                if freq_table[e] == 0:
+            if e in freq_map:
+                freq_map[e] -= 1
+                if freq_map[e] == 0:
                     counter -= 1
-            
             end += 1
 
-            #Now, when the counter is 0, prune the start
             while(counter == 0):
-                st = s[start]
-
-                if(end-start == l):
+                if len(p) == (end-start):
                     res.append(start)
                 
-                if st in freq_table:
-                    freq_table[st] += 1
-                    if freq_table[st] > 0:
+                st = s[start]
+                if st in freq_map:
+                    freq_map[st] += 1
+                    if freq_map[st] > 0:
                         counter += 1
-            
+                
                 start += 1
         
-
         return res
+
 
