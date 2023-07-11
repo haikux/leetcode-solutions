89bcfbadf3dd0604f9ac1577583c12673b048704
Haikux <harishrsk11@gmail.com>
2023-07-11 07:18:55 +0000

    Sync LeetCode submission - Minimum Window Substring (python3)

--- leetcode-solutions/problems/minimum_window_substring/solution.py
+++ leetcode-solutions/problems/minimum_window_substring/solution.py
@@ -1,49 +1,47 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        freq_table = {}
-        #Build frequency table
+        freq_map = {}
+
+        #Build freq map
         for i in t:
-            if i not in freq_table:
-                freq_table[i] = 1
+            if i in freq_map:
+                freq_map[i] += 1
             else:
-                freq_table[i] += 1
+                freq_map[i] = 1
         
         start, end = 0, 0
-        counter = len(freq_table)
-        l = float('inf')
+        counter = len(freq_map)
         res = ""
-        # Moving one part of the window right - end
+        l = float('inf')
+
+        if len(t) > len(s) or len(s) == 0:
+            return ""
+
         while(end < len(s)):
             e = s[end]
 
-            if e in freq_table:
-                freq_table[e] -= 1
-                if freq_table[e] == 0:
+            if e in freq_map:
+                freq_map[e] -= 1
+                if freq_map[e] == 0:
                     counter -= 1
             
             end += 1
-
-            # Once the window that satisfy the given condition
-            # is identenfied, identify the minimum window
-            # by moving 'begin'
-            while(counter == 0):
-                print(start)
-                b = s[start]
-
-                # update the minimum size
-                if (end-start) < l:
+            
+            # now start pruning the window from start to find minimum window
+            while (counter == 0):
+                if (end - start) < l:
                     l = end - start
                     res = s[start: end]
-                
-                # Prune the s at the left side and check if the counter is still 0
-                if b in freq_table:
-                    freq_table[b] += 1
-                    if freq_table[b] > 0:
+
+                st = s[start]
+                if st in freq_map:
+                    freq_map[st] += 1
+                    if freq_map[st] > 0:
                         counter += 1
-                    
                 
                 start += 1
         
         return res
+
 
 
