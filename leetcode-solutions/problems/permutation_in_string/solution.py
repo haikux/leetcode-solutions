c39784e036d3fd3ec05ab83a34000f32145f7478
Haikux <harishrsk11@gmail.com>
2023-07-13 07:26:00 +0000

    Sync LeetCode submission - Permutation in String (python3)

--- leetcode-solutions/problems/permutation_in_string/solution.py
+++ leetcode-solutions/problems/permutation_in_string/solution.py
@@ -1,17 +1,21 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         freq_map = {}
+        start, end = 0, 0
+        counter = 0
+        res = False
 
         for i in s1:
             if i in freq_map:
                 freq_map[i] += 1
             else:
                 freq_map[i] = 1
+                counter += 1
         
-        start, end = 0, 0
-        counter = len(freq_map)
-            
-        while end < len(s2):
+        if len(s1) < 1 or len(s1) > len(s2):
+            return res
+        
+        while(end < len(s2)):
             e = s2[end]
 
             if e in freq_map:
@@ -21,16 +25,18 @@ class Solution:
             
             end += 1
             while(counter == 0):
-                if (end - start) == len(s1):
+                if (end-start) == len(s1):
                     return True
+                
                 st = s2[start]
-
                 if st in freq_map:
                     freq_map[st] += 1
                     if freq_map[st] > 0:
                         counter += 1
                 start += 1
             
-            
-        
         return False
+            
+    
+
+        
