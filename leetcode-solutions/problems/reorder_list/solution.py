04227c988a56d8e23614841f2761e21e511bf543
Haikux <harishrsk11@gmail.com>
2023-07-30 09:08:37 +0000

    Sync LeetCode submission - Reorder List (python3)

--- leetcode-solutions/problems/reorder_list/solution.py
+++ leetcode-solutions/problems/reorder_list/solution.py
@@ -8,26 +8,33 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        # Finding mid
-        slow, fast = head, head.next
+        # Generate two lists
+        # Reverse the second list
+        # Merge two lists
+        slow = head
+        fast = head.next
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
 
-        # Reverse second half
-        second = slow.next
-        prev = slow.next = None
-        while second:
-            tmp = second.next
-            second.next = prev
-            prev = second
-            second = tmp
+        curr1 = head
+        curr2 = slow
+
+        # Reverse second list
+        prev = None
+        while curr2:
+            tmp = curr2.next
+            curr2.next = prev
+            prev = curr2
+            curr2 = tmp
         
-        # Merge two halves
-        first, second = head, prev
-        while second:
-            tmp1, tmp2 = first.next, second.next
-            first.next = second
-            second.next = tmp1
-            first, second = tmp1, tmp2
+        curr2 = prev
+        # Merging two lists
+        while curr1 and curr2:
+            tmp1, tmp2 = curr1.next, curr2.next
+            curr1.next = curr2
+            curr2.next = tmp1
+            curr1, curr2  = tmp1, tmp2
             
+
+
