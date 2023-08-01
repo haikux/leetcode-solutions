6a0e199b5a920de1dca2d1b983822fd701a2a0ff
Haikux <harishrsk11@gmail.com>
2023-08-01 01:54:52 +0000

    Sync LeetCode submission - LRU Cache (python3)

--- leetcode-solutions/problems/lru_cache/solution.py
+++ leetcode-solutions/problems/lru_cache/solution.py
@@ -7,12 +7,12 @@ class Node:
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cap = capacity
+        self.capacity = capacity
         self.map = {}
 
-        self.left = self.right = Node(0, None)
-        self.left.next, self.right.prev =  self.right, self.left
-    
+        self.left = self.right = Node(0,0)
+        self.left.next, self.right.prev = self.right, self.left
+
     def remove(self, node):
         prev, nxt = node.prev, node.next
         prev.next, nxt.prev = nxt, prev
@@ -24,23 +24,29 @@ class LRUCache:
 
     def get(self, key: int) -> int:
         if key in self.map:
+            # Since the key is accessed
+            # Update the location - remove the current one
+            # add to the right
             self.remove(self.map[key])
             self.insert(self.map[key])
             return self.map[key].val
         return -1
 
     def put(self, key: int, value: int) -> None:
+        # if the key already exists, remove the exisiting one
+        # coz the value is being updated - remove the reference
+        # from the map
         if key in self.map:
             self.remove(self.map[key])
-        
+        # Now create a new node and add to the map
         self.map[key] = Node(key, value)
         self.insert(self.map[key])
 
-        if len(self.map) > self.cap:
+        # If the capacity is exceeded, then remove the LRU
+        if len(self.map) > self.capacity:
             n = self.left.next
             self.remove(n)
             del self.map[n.key]
-        
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
