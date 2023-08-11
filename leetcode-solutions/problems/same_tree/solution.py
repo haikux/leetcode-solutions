4278a7e9be3c30ad7fe6076abd610891fa0cddbf
Haikux <harishrsk11@gmail.com>
2023-08-11 20:03:30 +0000

    Sync LeetCode submission - Same Tree (python3)

--- leetcode-solutions/problems/same_tree/solution.py
+++ leetcode-solutions/problems/same_tree/solution.py
@@ -9,6 +9,7 @@ class Solution:
         if not p and not q:
             return True
         if p and q and p.val == q.val:
-            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+            return self.isSameTree(p.left, q.left) \
+                and self.isSameTree(p.right, q.right)
         else:
             return False
