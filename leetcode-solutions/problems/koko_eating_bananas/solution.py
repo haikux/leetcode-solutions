2ead381067c6f00994abd76bbb9d0927f9699d51
Haikux <harishrsk11@gmail.com>
2023-08-15 21:33:28 +0000

    Sync LeetCode submission - Koko Eating Bananas (python3)

--- leetcode-solutions/problems/koko_eating_bananas/solution.py
+++ leetcode-solutions/problems/koko_eating_bananas/solution.py
@@ -1,6 +1,10 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # Idea is to first come up with the solution space
+        # in this case, k minimum is 1 and max could be max(piles)
         top, bot = 1, max(piles)
+
+        # Start with the max possible value
         result = bot
         while top <= bot:
             mid = (top + bot) // 2
