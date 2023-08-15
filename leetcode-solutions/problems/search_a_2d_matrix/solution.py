2bb20902bda23e4fa64a42ac169010767b98be30
Haikux <harishrsk11@gmail.com>
2023-08-15 19:03:00 +0000

    Sync LeetCode submission - Search a 2D Matrix (python3)

--- leetcode-solutions/problems/search_a_2d_matrix/solution.py
+++ leetcode-solutions/problems/search_a_2d_matrix/solution.py
@@ -1,32 +1,34 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        rs, re = 0, len(matrix)-1
-        cs, ce = 0, len(matrix[0])-1
-        #print(cs, ce)
+        # To solve it in O(log(m*n))
+        # Perform 2 binary searches
+        # First on the rows, to find the rows where the target could potentially be there
+        # Second, once the rows are id'd BS on column to find the target value
 
+        rs, re = 0, len(matrix)-1
 
         while rs <= re:
-            #print(rs, re)
-            mid = rs + (re - rs) // 2
+            mid = (rs + re) // 2
             if target < matrix[mid][0]:
                 re = mid - 1
             elif target > matrix[mid][-1]:
                 rs = mid + 1
             else:
+                # Value should be there in the current mid row
                 break
-
-        rs = rs + (re - rs) // 2
-        print("RS ",rs)
-        if not (rs <= re):
+        
+        # if the loop did not break but exited due to while loop condition
+        if rs > re:
             return False
+        mid = (rs + re) // 2
+
+        cs, ce = 0, len(matrix[mid])-1
         while cs <= ce:
-            print(cs, ce)
-            mmid = cs + (ce-cs) // 2
-            #print(matrix[rs][mmid])
-            if target < matrix[rs][mmid]:
-                ce = mmid - 1
-            elif target > matrix[rs][mmid]:
-                cs = mmid + 1
+            m = (cs + ce) // 2 
+            if target > matrix[mid][m]:
+                cs = m + 1
+            elif target < matrix[mid][m]:
+                ce = m - 1
             else:
                 return True
         return False
