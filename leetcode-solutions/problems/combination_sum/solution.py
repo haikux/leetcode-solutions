ab12d2eb26395b5a6ef6f008b5ffd5ae6dac76cc
Haikux <harishrsk11@gmail.com>
2023-08-31 04:28:32 +0000

    Sync LeetCode submission - Combination Sum (python3)

--- leetcode-solutions/problems/combination_sum/solution.py
+++ leetcode-solutions/problems/combination_sum/solution.py
@@ -1,17 +1,19 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        self.sub = []
-        self.sup = []
-        def csum(j, target):
+        # Time O(n*2^n)
+        # Space O(n + 2^n)
+        result = []
+        sub = []
+
+        def dfs(j, target):
             if target == 0:
-                self.sup.append(self.sub.copy())
-                return
+                return result.append(sub.copy())
             if target < 0:
                 return
             for i in range(j, len(candidates)):
-                self.sub.append(candidates[i])
-                csum(i, target-candidates[i])
-                self.sub.pop()
-        
-        csum(0, target)
-        return self.sup
+                sub.append(candidates[i])
+                dfs(i, target - candidates[i])
+                sub.pop()
+
+        dfs(0, target)
+        return result
