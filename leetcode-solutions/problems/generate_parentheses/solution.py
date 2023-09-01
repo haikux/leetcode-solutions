77a929ae9ee233f6714cc0bea6f9c95bca7c1332
Haikux <harishrsk11@gmail.com>
2023-09-01 16:08:37 +0000

    Sync LeetCode submission - Generate Parentheses (python3)

---
+++ leetcode-solutions/problems/generate_parentheses/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+        res = []
+
+        def bktrack(o, c):
+            if o == c == n:
+                result.append(''.join(res.copy()))
+            if o < n:
+                res.append("(")
+                bktrack(o+1, c)
+                res.pop()
+            if c < o:
+                res.append(")")
+                bktrack(o, c+1)
+                res.pop()
+        
+        bktrack(0,0)
+        return result
+
